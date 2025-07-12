3e33d01d1a5f6e41cd8c00e90dd8cc17911badbc
dhruvpapdeja06 <dhruv2024papdeja@gmail.com>
2025-07-13 00:45:37 +0530

    update in jenkin-rhel file

--- jenkins_rhel9.sh
+++ jenkins_rhel9.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # setup jenkins repo
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
@@ -11,7 +12,3 @@ sudo yum install fontconfig java-21-openjdk -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 
-[jenkins]
-name=Jenkins-stable
-baseurl=http://pkg.jenkins.io/redhat-stable
-gpgcheck=1
